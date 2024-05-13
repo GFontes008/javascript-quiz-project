4bf05bbf97cfc568f00d3f7959dfc3fc4275148f
Ana Rosa <guilherme.fontes98@outlook.com>
2024-05-13 16:43:06 +0100

    first commit

--- src/question.js
+++ src/question.js
@@ -3,5 +3,29 @@ class Question {
     //
     // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+    constructor (text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+
+   
+// 2. shuffleChoices()
+
+ shuffleChoices() { 
+    
+    for (let i = 0; i < this.choices.length; i++){ 
+        let randomIndex = Math.floor(Math.random() * this.choices.length);
+        let randomElement = this.choices[randomIndex];
+        this.choices.splice(randomIndex, 1);
+        this.choices.push(randomElement);
+    } 
 }
+}   
+
+
+    
+     
+   
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,36 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+    getQuestion () {
+        return this.questions[this.currentQuestionIndex];
+    }
+
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+
+    shuffleQuestions() {
+        for (let i = 0; i < this.questions.length; i++){ 
+            let randomIndex = Math.floor(Math.random() * this.questions.length);
+            let randomElement = this.questions[randomIndex];
+            this.questions.splice(randomIndex, 1);
+            this.questions.push(randomElement);
+        } 
+   
+}
+       
 
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
+ }

cad6ac2388c1eb877dba04972f54f2f9315fbc39
Ana Rosa <guilherme.fontes98@outlook.com>
2024-05-14 16:38:21 +0100

    day 2 interection

--- src/quiz.js
+++ src/quiz.js
@@ -1,46 +1,59 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor (questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  shuffleQuestions() {
+    for (let i = 0; i < this.questions.length; i++) {
+      let randomIndex = Math.floor(Math.random() * this.questions.length);
+      let randomElement = this.questions[randomIndex];
+      this.questions.splice(randomIndex, 1);
+      this.questions.push(randomElement);
     }
-    // 2. getQuestion()
+  }
 
-    getQuestion () {
-        return this.questions[this.currentQuestionIndex];
+  checkAnswer(answer) {
+    this.correctAnswers++;
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
     }
-
-
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
+  }
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
     }
+  }
 
-    shuffleQuestions() {
-        for (let i = 0; i < this.questions.length; i++){ 
-            let randomIndex = Math.floor(Math.random() * this.questions.length);
-            let randomElement = this.questions[randomIndex];
-            this.questions.splice(randomIndex, 1);
-            this.questions.push(randomElement);
-        } 
-   
+  averageDifficulty() {
+    const sumDifficulty = this.questions.reduce(
+      (acc, curr) => acc + curr.difficulty,
+      0
+    );
+
+    const averageDifficulty = sumDifficulty / this.questions.length;
+
+    return averageDifficulty;
+  }
 }
-       
-    checkAnswer (answer) {
-       this.correctAnswers++;
-    }
-
-    hasEnded(){
-        if (this.currentQuestionIndex < this.questions.length) {
-            return false;
-        }
-        else{
-                return true;
-            }
-        } 
-    }
- 
